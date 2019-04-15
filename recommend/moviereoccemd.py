032ee686939041d4ca52ab4baaf2d9fc70639fa5
psychic <psychic@secuve.com>
2019-04-15 15:43:19 +0900

    recommend

--- recommend/moviereoccemd.py
+++ recommend/moviereoccemd.py
@@ -8,8 +8,9 @@ import numpy as np
 import pandas as pd
 import json
 import time
-import os
+import os, sys
 import pickle
+# from sklearn.externals import joblib
 
 
 def parse_genres(genres_str):
@@ -22,7 +23,6 @@ def parse_genres(genres_str):
 
 
 meta = pd.read_csv('dataset/movies_metadata.csv')
-print(meta.head())
 meta=meta[['id', 'original_title', 'original_language', 'genres']]
 meta=meta.rename(columns={'id':'movieId'})
 meta=meta[meta['original_language']=='en']
@@ -31,24 +31,61 @@ meta.movieId = pd.to_numeric(meta.movieId, errors='coerce')
 print(meta.head())
 
 
+# input_movie = 'Zodiac'
+# input_movie = 'Heat'
+# input_movie = 'Toy Story'
+input_movie = 'The Dark Knight'
+# input_movie='fast and the furious'
+# input_movie='beauty and the beast'
+
+foundi=-1
+for i, x in enumerate(meta['original_title']):
+    ot = x.strip().upper()
+    if ot.find(input_movie.upper())>=0:
+        foundi=i
+        print(i,'movieId=', meta['movieId'].values[i], 'title=', x)
+        break
+
+if foundi<0:
+    print('not found movie')
+    sys.exit()
+else:
+    print('found movie index=', i, 'title=', x, 'len=',len(x))
+    input_movie = x
+
+print('genres=', meta[meta['original_title']==input_movie]['genres'])
+
+
+if meta[meta['original_title']==input_movie]['genres'].count()==0:
+    print('not found')
+    sys.exit(0)
+
 if os.path.exists('dataset/rating.pkl'):
+    print('load matrix...')
     with open('dataset/rating.pkl', 'rb') as fp:
         matrix = pickle.load(fp)
+    # matrix = joblib.load('dataset/rating.job')
+    print(matrix.head(20))
+    # sys.exit()
 else:
-    # ratings = pd.read_csv('dataset/ratings_small.csv')
-    ratings = pd.read_csv('dataset/ratings.csv')
+    ratings = pd.read_csv('dataset/ratings_small.csv')
+    # ratings = pd.read_csv('dataset/ratings.csv')
     ratings = ratings[['userId', 'movieId', 'rating']]
     ratings.movieId = pd.to_numeric(ratings.movieId, errors='coerce')
     print(ratings.head())
     print( ratings.describe() )
-
+    print('making matrix...')
     data = pd.merge(ratings, meta, on='movieId', how='inner')
+    print('data head')
+    print(data.head())
     matrix = data.pivot_table(index='userId', columns='original_title', values='rating')
+    print('matrix head')
+    print(matrix.head())
+    print('save matrix...')
     with open('dataset/rating.pkl', 'wb') as fp:
-        pickle.dump(matrix, fp)
+        pickle.dump(matrix, fp, protocol=4)
+    # joblib.dump(matrix, 'dataset/rating.job')
 
-
-print(matrix.head(20))
 
 GENRE_WEIGHT=0.1
 
@@ -94,10 +131,6 @@ def recommend(input_movie, matrix, n, similar_genre=True):
 
 print('predict')
 
-# input_movie = 'Zodiac'
-# input_movie = 'Heat'
-# input_movie = 'Toy Story'
-input_movie = 'Sleepness in Seattle'
 print('genres=', meta[meta['original_title']==input_movie]['genres'])
 recommend_result = recommend(input_movie, matrix, 10, similar_genre=True)
 # recommend movie
