aef7c14b41b9c089a5275a23363327bc8aa897ab
crazyj7 <crazyj7@gmail.com>
2019-04-16 16:17:00 +0900

    cat

--- cv/cat_hipster.py
+++
@@ -1,7 +0,0 @@
-import keras, datetime
-from keras.layers import Input, Dense
-from keras.models import Model
-from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau
-from keras.applications import mobilenetv2
-import numpy as np
-
