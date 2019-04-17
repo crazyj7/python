22530fe19350e1afa110e7fdf4d8fb6d76f7fb7e
psychic <psychic@secuve.com>
2019-04-17 15:57:59 +0900

    update train

--- cv/cat_hipster_train2.py
+++ cv/cat_hipster_train2.py
@@ -5,6 +5,9 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau
 from keras.applications import mobilenetv2
 import numpy as np
 
+import os
+from keras.models import load_model
+
 img_size = 224
 
 modelsavefile = 'cat_hipster2.model'
@@ -12,12 +15,19 @@ output_size=18  #9points
 
 data_00 = np.load('./cats/CAT_00_2.npy')
 data_01 = np.load('./cats/CAT_01_2.npy')
+data_02 = np.load('./cats/CAT_02_2.npy')
+data_03 = np.load('./cats/CAT_03_2.npy')
+
 data_06 = np.load('./cats/CAT_06_2.npy')
 
 x_train = np.concatenate( (data_00.item().get('img2'),
-                           data_01.item().get('img2')), axis=0)
+                           data_01.item().get('img2'),
+                           data_02.item().get('img2'),
+                           data_03.item().get('img2')), axis=0)
 y_train = np.concatenate( (data_00.item().get('lmk2'),
-                           data_01.item().get('lmk2')), axis=0)
+                           data_01.item().get('lmk2'),
+                           data_02.item().get('lmk2'),
+                           data_03.item().get('lmk2')), axis=0)
 
 x_test = np.array( data_06.item().get('img2'))
 y_test = np.array( data_06.item().get('lmk2'))
@@ -34,22 +44,27 @@ y_test = np.reshape( y_test, (-1, output_size))
 
 inputs = Input(shape=(img_size, img_size, 3))
 
-mobilenetv2_model = mobilenetv2.MobileNetV2(input_shape=(img_size, img_size, 3),
-                      alpha=1.0, include_top=False, weights='imagenet',
-                    input_tensor=inputs, pooling='max')
+if os.path.exists(modelsavefile):
+    model = load_model(modelsavefile)
+    print('continous train....')
+else:
+    mobilenetv2_model = mobilenetv2.MobileNetV2(input_shape=(img_size, img_size, 3),
+                          alpha=1.0, include_top=False, weights='imagenet',
+                        input_tensor=inputs, pooling='max')
 
-net = Dense(128, activation='relu')(mobilenetv2_model.layers[-1].output)
-net = Dense(64, activation='relu')(net)
-net = Dense(output_size, activation='linear')(net)
+    net = Dense(128, activation='relu')(mobilenetv2_model.layers[-1].output)
+    net = Dense(64, activation='relu')(net)
+    net = Dense(output_size, activation='linear')(net)
 
-model = Model(inputs=inputs, outputs=net)
+    model = Model(inputs=inputs, outputs=net)
 
-model.summary()
+    model.summary()
 
-# train
-model.compile(optimizer=keras.optimizers.Adam(), loss='mse')
+    # train
+    model.compile(optimizer=keras.optimizers.Adam(), loss='mse')
 
-model.fit(x_train, y_train, epochs=50, batch_size=32, shuffle=True,
+# epoch 50
+model.fit(x_train, y_train, epochs=5, batch_size=32, shuffle=True,
           validation_data=(x_test, y_test), verbose=1)
 
 model.save(modelsavefile)
