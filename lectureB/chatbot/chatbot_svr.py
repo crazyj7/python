e6e45564cdd4dfd7bd3d7f0e4ac3620d16c66921
crazyj7 <crazyj7@gmail.com>
2019-05-02 20:20:46 +0900

    update bot

--- lectureB/chatbot/chatbot_svr.py
+++ lectureB/chatbot/chatbot_svr.py
@@ -2,7 +2,8 @@
 from telegram.ext import Updater, MessageHandler, Filters
 
 from emoji import emojize
-from daum_top10 import get_daum_top10
+from daum_top10 import getTop10Daum
+from weather01 import getWeather
 
 apikey = '848818498:AAGvUjAKHoB4Qnl6AynzKeLpJNT6ed2dLkE'
 
@@ -10,7 +11,17 @@ updater = Updater(token=apikey)
 dispatcher = updater.dispatcher
 updater.start_polling()
 
-get_daum_top10()
+
+def weather():
+    msg = getWeather()
+    return msg
+
+def top10():
+    top10daum = getTop10Daum()
+    msg=''
+    for iss in top10daum:
+        msg += "{}위 {} ".format(iss['order'], iss['title'])
+    return msg
 
 
 def handler(bot, update):
@@ -23,6 +34,11 @@ def handler(bot, update):
         bot.send_message(chat_id=chat_id, text=emojize('아잉:heart_eyes:', use_aliases=True))
     elif '사진' in text:
         bot.send_photo(chat_id=chat_id, photo=open('photo.jpg', 'rb'))
+    elif '검색어' in text or '이슈' in text:
+        bot.send_message(chat_id=chat_id, text='지금 실시간 이슈는...')
+        bot.send_message(chat_id=chat_id, text=top10())
+    elif '날씨' in text:
+        bot.send_message(chat_id=chat_id, text=weather())
     else:
         bot.send_message(chat_id=chat_id, text='몰라')
 
