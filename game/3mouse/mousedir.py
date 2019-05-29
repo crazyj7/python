5d246ebbd441097e2f8e419d1bfd040344801abc
psychic <psychic@secuve.com>
2019-05-29 10:25:40 +0900

    comment

--- game/3mouse/mousedir.py
+++ game/3mouse/mousedir.py
@@ -19,26 +19,46 @@ pad = pygame.display.set_mode( (640,480))
 pygame.display.set_caption('test')
 
 user = Surface((100,100))
+# 시작 이미지. x축으로 0도 기울기.
 pygame.draw.polygon(user, RED, [(0,0), (100,50), (0,100)], 3)
 pygame.draw.line(user, GREEN, (100,50), (0, 50), 2)
+pygame.draw.rect(user, BLUE, pygame.Rect(0, 0, 100, 100), 2)
 user.set_colorkey(BLACK)
 pos = user.get_rect()
+# 시작 위치
 pos.centerx = 100
 pos.centery = 100
-print('pos (rect)= ', pos, ' current angle=0')
+# print('pos (rect)= ', pos, ' current angle=0')
+
 
 def rot_center2(image, angle):
-    """rotate an image while keeping its center and size. 크기 유지. """
+    '''
+    사각 영역은 변함없고, 내부의 이미지만 회전 시키고, 밖으로 나간 부분은 잘린다. 중심유지.
+    :param image:
+    :param angle:
+    :return:
+    '''
     orig_rect = image.get_rect()
+    # 이미지 회전
     rot_image = pygame.transform.rotate(image, angle)
     rot_rect = orig_rect.copy()
+    # 원본 이미지 크기의 중심을 회전된 이미지 영역의 중심에 위치
     rot_rect.center = rot_image.get_rect().center
+    # 원본 이미지 크기만큼 자름.
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
 
 def rot_center(image, rect, angle):
-    """rotate an image while keeping its center. 크기 잘림."""
+    '''
+    영역의 중심점에서 회전시키고, 새로운(더 커진) 영역 크기도 반환. 잘림 없음. 중심유지.
+    :param image:
+    :param rect:
+    :param angle:
+    :return:
+    '''
+    # 각도 만큼 회전.
     rot_image = pygame.transform.rotate(image, angle)
+    # 중심점을 맞춘다. 새로운 영역 보정. 영역 크기가 커질수 있음. 짤림 없음.
     rot_rect = rot_image.get_rect(center=rect.center)
     return rot_image, rot_rect
 
