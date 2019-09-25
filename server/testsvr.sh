31e434405f4acabffefa36f4ddd8491159fe44dd
crazyj7 <crazyj7@gmail.com>
2019-09-25 11:12:19 +0900

    testsvr

---
+++ server/testsvr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# [python env path...]/bin/python testsvr.py
+# linux ex)
+/opt/anaconda3/envs/tensorflow/bin/python testsvr.py
+# windows command ex)
+# c:\python37\scripts\python testsvr.py
+
