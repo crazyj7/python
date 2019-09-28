2b301cea4fe7ef26525d03c1d2229d18d5cf1795
crazyj7 <crazyj7@gmail.com>
2019-09-28 10:13:47 +0900

    up

--- kaggle/kannadamnist/kannadaMNIST analysis.py
+++ kaggle/kannadamnist/kannadaMNIST analysis.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+# # KannadaMNIST analysis
 
-# In[ ]:
-
-
+# +
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in 
@@ -21,10 +20,7 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 
 # Any results you write to the current directory are saved as output.
 
-
-# In[ ]:
-
-
+# +
 # 경고 무시 (쓸데없는 로그 출력금지)
 import warnings
 warnings.filterwarnings('ignore')
@@ -58,9 +54,7 @@ from sklearn.model_selection import StratifiedKFold
 from keras.preprocessing.image import ImageDataGenerator
 import gc
 
-########################
-# Seed All
-# import numpy as np
+# +
 import tensorflow as tf
 import random
 # import os
@@ -69,143 +63,6 @@ import warnings
 from keras.callbacks import Callback
 from datetime import datetime
 from pytz import timezone, utc
-
-
-# In[ ]:
-
-
-## load data
-inputdir='../input/Kannada-MNIST/'
-outputdir='./'
-datadir='../input/mykannada2/'
-# datadir='/kaggle/input/mykannada/'
-
-
-dftrain = pd.read_csv(inputdir+'train.csv')
-dftest = pd.read_csv(inputdir+'test.csv')
-dfsub = pd.read_csv(inputdir+'sample_submission.csv')
-
-
-# In[ ]:
-
-
-dftrain.head()
-
-
-# In[ ]:
-
-
-dftest.head()
-
-
-# In[ ]:
-
-
-nptrain = np.asarray(dftrain.iloc[:,1:].values)
-print('test shape=', nptrain.shape)
-
-nptest = np.asarray(dftest.iloc[:,1:].values)
-print('test shape=', nptest.shape)
-
-
-# In[ ]:
-
-
-nptrain=nptrain.reshape((-1,28,28,1))
-print('train shape=', nptrain.shape)
-nptest=nptest.reshape((-1,28,28,1))
-print('test shape=', nptest.shape)
-
-
-# In[ ]:
-
-
-def image_show(npdata, labels, cnt, brandom=True):
-    plt.figure(figsize=(6*cnt, 6))
-    if brandom:
-        idx=np.random.randint(0,npdata.shape[0], cnt)
-    else:
-        idx=np.arange(cnt)
-    for i in range(cnt):
-        plt.subplot(1,cnt, i+1)
-        plt.title(labels[idx[i]])
-        imgdata = npdata[idx[i]].squeeze()
-        plt.imshow(imgdata, cmap='gray')
-    plt.show()
-
-
-# In[ ]:
-
-
-# train 이미지 랜덤 보기
-if False:
-    image_show(nptrain, dftrain.label, 6, True)
-
-
-# In[ ]:
-
-
-# test 이미지 보기.
-if False:
-    image_show(nptest, dftest.id, 6, False)
-
-
-# In[ ]:
-
-
-dftrain.label.value_counts()
-
-
-# ## Image Augument
-
-# In[ ]:
-
-
-datagen1 = ImageDataGenerator(rescale=1./255, shear_range=0.1, zoom_range=0.1, 
-                              horizontal_flip=False, vertical_flip=False,
-                              width_shift_range=0.1, height_shift_range=0.1, 
-                              rotation_range=15, brightness_range=[0.5, 1.2],
-                              fill_mode='nearest')
-datagen2 = ImageDataGenerator(rescale=1./255)
-
-if False:
-    datagen1.fit(nptrain)
-    for xbatch, ybatch in datagen1.flow(nptrain, dftrain['label'], batch_size=6, shuffle=True):
-        image_show(xbatch, ybatch, 6, False)
-        break
-
-
-# In[ ]:
-if False:
-    for xbatch, ybatch in datagen2.flow(nptrain, dftrain['label'], batch_size=6, shuffle=False):
-        image_show(xbatch, ybatch, 6, False)
-        break
-
-
-
-datagen3 = ImageDataGenerator(rescale=1./255, shear_range=0.1, zoom_range=0.1,
-                              horizontal_flip=False, vertical_flip=False,
-                              width_shift_range=0.1, height_shift_range=0.1,
-                              rotation_range=15, brightness_range=[0.5, 1.2],
-                              fill_mode='nearest')
-samplex = nptrain[0:5]
-sampley = dftrain['label'][0:5]
-print('sampley=', sampley)
-if False:
-    i=0
-    for xbatch, ybatch in datagen3.flow(samplex, sampley, batch_size=5, shuffle=False):
-        print(xbatch.shape, ybatch.shape)
-        i+=1
-        image_show(xbatch, ybatch, xbatch.shape[0], False)
-        if i==3:
-            break
-
-
-
-# ## Train
-# 
-
-# In[ ]:
 
 
 KST = timezone('Asia/Seoul')
@@ -237,12 +94,112 @@ def seed_All():
 
 seed_All()
 
+# +
+## load data
+inputdir='../input/Kannada-MNIST/'
+outputdir='./'
+datadir='../input/mykannada2/'
+# datadir='/kaggle/input/mykannada/'
 
-# In[ ]:
+
+dftrain = pd.read_csv(inputdir+'train.csv')
+dftest = pd.read_csv(inputdir+'test.csv')
+dfadd = pd.read_csv(inputdir+'Dig-MNIST.csv')
+dfsub = pd.read_csv(inputdir+'sample_submission.csv')
+# -
+
+dftrain = dftrain.append(dfadd, ignore_index=True)
+
+dftrain.label.value_counts()
 
 
-###############################################
+dftest.head()
+
+# +
+nptrain = np.asarray(dftrain.iloc[:,1:].values)
+print('train shape=', nptrain.shape)
+
+nptest = np.asarray(dftest.iloc[:,1:].values)
+print('test shape=', nptest.shape)
+# -
+
+nptrain=nptrain.reshape((-1,28,28,1))
+print('train shape=', nptrain.shape)
+nptest=nptest.reshape((-1,28,28,1))
+print('test shape=', nptest.shape)
+
+
+def image_show(npdata, labels, cnt, brandom=True):
+    plt.figure(figsize=(6*cnt, 6))
+    if brandom:
+        idx=np.random.randint(0,npdata.shape[0], cnt)
+    else:
+        idx=np.arange(cnt)
+    for i in range(cnt):
+        plt.subplot(1,cnt, i+1)
+        plt.title(labels[idx[i]])
+        imgdata = npdata[idx[i]].squeeze()
+        plt.imshow(imgdata, cmap='gray')
+    plt.show()
+
+
+# train 이미지 랜덤 보기
+if True:
+    image_show(nptrain, dftrain.label.values, 6, True)
+
+
+# test 이미지 보기.
+if False:
+    image_show(nptest, dftest.id, 6, False)
+
+
+dftrain.label.value_counts()
+
+# ## Image Augument
+
+# +
+datagen1 = ImageDataGenerator(rescale=1./255, shear_range=0.1, zoom_range=0.1, 
+                              horizontal_flip=False, vertical_flip=False,
+                              width_shift_range=0.1, height_shift_range=0.1, 
+                              rotation_range=15, brightness_range=[0.5, 1.2],
+                              fill_mode='nearest')
+datagen2 = ImageDataGenerator(rescale=1./255)
+
+if False:
+    datagen1.fit(nptrain)
+    for xbatch, ybatch in datagen1.flow(nptrain, dftrain['label'], batch_size=6, shuffle=True):
+        image_show(xbatch, ybatch, 6, False)
+        break
+
+
+if False:
+    for xbatch, ybatch in datagen2.flow(nptrain, dftrain['label'], batch_size=6, shuffle=False):
+        image_show(xbatch, ybatch, 6, False)
+        break
+# -
+
+datagen3 = ImageDataGenerator(rescale=1./255, shear_range=0.1, zoom_range=0.1,
+                              horizontal_flip=False, vertical_flip=False,
+                              width_shift_range=0.1, height_shift_range=0.1,
+                              rotation_range=15, brightness_range=[0.5, 1.2],
+                              fill_mode='nearest')
+samplex = nptrain[0:5]
+sampley = dftrain['label'][0:5]
+print('sampley=', sampley)
+if False:
+    i=0
+    for xbatch, ybatch in datagen3.flow(samplex, sampley, batch_size=5, shuffle=False):
+        print(xbatch.shape, ybatch.shape)
+        i+=1
+        image_show(xbatch, ybatch, xbatch.shape[0], False)
+        if i==3:
+            break
+
+# ## Train
+
+# +
 # configure
+# + {}
 bDebug=False
 
 # running mode
@@ -255,24 +212,24 @@ RUNMODE_TEST_ONE=2
 RUNMODE_TEST_ALL=3
 RUNMODE_TEST_ALL_TTA=4
 
-runmode = RUNMODE_TEST_ALL_TTA
+runmode = RUNMODE_TRAIN_ALL
 
 
 epochs = 70
 
-################################################
+# +
 # !!! curFold 1~6 make 6 models...
+# + {}
 curFold = 1  # make current fold (1..fold_k)
 
-batch_size=32  # 16, 32, 64  debug.. memory dependent!
+# batch_size=32  # 16, 32, 64  debug.. memory dependent!
+batch_size=64
 # K fold
-fold_k = 6
+fold_k = 4
 imagesize=28
 
 
-# In[ ]:
-
-
+# +
 if bDebug:
     print('Debug Version!!!! ')
     print('Fast Fast. Only use 1000 items in train.')
@@ -280,15 +237,12 @@ if bDebug:
     nptrain = nptrain[:1000]
     epochs = 10
 
-# In[ ]:
-
 method = 'conv'
 modelname = 'kannada-v1-'
 modellist = [modelname+'1-', modelname+'2-', modelname+'3-', 
-             modelname+'4-', modelname+'5-', modelname+'6-']
+             modelname+'4-']
+#  , modelname+'5-', modelname+'6-'
 
-
-# In[ ]:
 if runmode==RUNMODE_TEST_ONE:
     # test model number? 1~6
     files = glob.glob(datadir+modelname+str(curFold)+'-*')
@@ -298,20 +252,18 @@ if runmode==RUNMODE_TEST_ONE:
     print('modelpath=', modelpath)
 
 
-# In[ ]:
-
-
 skf = StratifiedKFold(fold_k, random_state=SEED)
 
+# -
 
-# In[ ]:
+dftrain.iloc[0:5]['label']
 
-
+# +
 if runmode==RUNMODE_TRAIN_ONE or runmode==RUNMODE_TRAIN_ALL:
     # train
    
     kk=0
-    for modelname, (tri, tei) in zip(modellist, skf.split(nptrain, dftrain['label'])):
+    for modelname, (tri, tei) in zip(modellist, skf.split(nptrain, dftrain['label'].values)):
         kk+=1
         if runmode==RUNMODE_TRAIN_ALL:
             curFold = kk
@@ -327,9 +279,10 @@ if runmode==RUNMODE_TRAIN_ONE or runmode==RUNMODE_TRAIN_ALL:
 
         nptrain_t = nptrain[tri,:]
         nptrain_v = nptrain[tei,:]
+        dbgprint('traincnt={} valcnt={}'.format(len(tri), len(tei)))
 
-        dflabel_t = np_utils.to_categorical(dftrain.loc[tri]['label'])
-        dflabel_v = np_utils.to_categorical(dftrain.loc[tei]['label'])
+        dflabel_t = np_utils.to_categorical(dftrain.iloc[tri]['label'])
+        dflabel_v = np_utils.to_categorical(dftrain.iloc[tei]['label'])
 
         datagen1 = ImageDataGenerator(rescale=1./255, shear_range=0.1, zoom_range=0.1, 
                                       horizontal_flip=False, vertical_flip=False,
@@ -364,25 +317,25 @@ if runmode==RUNMODE_TRAIN_ONE or runmode==RUNMODE_TRAIN_ALL:
                          input_shape = (28,28,1)))
         model.add(Conv2D(filters = 32, kernel_size = (4,4),padding = 'Same', activation ='relu'))
         model.add(MaxPool2D(pool_size=(2,2)))
-        model.add(Dropout(0.25))
+        model.add(Dropout(rate=0.25))
 
         model.add(Conv2D(filters = 64, kernel_size = (4,4),padding = 'Same', activation ='relu'))
         model.add(MaxPool2D(pool_size=(2,2)))
-        model.add(Dropout(0.25))
+        model.add(Dropout(rate=0.25))
 
         model.add(Conv2D(filters = 128, kernel_size = (4,4),padding = 'Same', activation ='relu'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(rate=0.25))
 
         model.add(Conv2D(filters = 256, kernel_size = (4,4),padding = 'Same', activation ='relu'))
         model.add(MaxPool2D(pool_size=(2,2)))
-        model.add(Dropout(0.25))
+        model.add(Dropout(rate=0.25))
 
         model.add(Conv2D(filters = 128, kernel_size = (4,4),padding = 'Same', activation ='relu'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(rate=0.25))
 
         model.add(Flatten())
         model.add(Dense(256, activation='relu'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(rate=0.25))
         model.add(Dense(10, activation='softmax'))
 
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
@@ -396,8 +349,8 @@ if runmode==RUNMODE_TRAIN_ONE or runmode==RUNMODE_TRAIN_ALL:
 
         # model.save(outputdir+'model.h5')   # create last train model file in CWD
 
-
-# In[ ]:
+# +
+# # +
 
 if runmode==RUNMODE_TEST_ONE:
     print('RUNMODE_TEST_ONE')
@@ -423,8 +376,8 @@ if runmode==RUNMODE_TEST_ONE:
     print(dftrain['label'].values)
     print('match cnt=', np.sum(predict_result==dftrain['label'].values))    
 
-
-# In[ ]:
+# +
+# # +
 
 
 if runmode==RUNMODE_TEST_ONE:
@@ -438,9 +391,9 @@ if runmode==RUNMODE_TEST_ONE:
     dfsub['label'] = predict_result
     dfsub.to_csv('submit.csv', index=False)
 
-# In[ ]:
+# # +
 
-
+# +
 if runmode==RUNMODE_TEST_ALL:
     print("RUNMODE_TEST_ALL")
     datagen1 = ImageDataGenerator(rescale=1. / 255)
@@ -512,7 +465,3 @@ elif runmode==RUNMODE_TEST_ALL_TTA:
 
     dfsub['label'] = predict_result
     dfsub.to_csv('submit.csv', index=False)
-
-
-
-
