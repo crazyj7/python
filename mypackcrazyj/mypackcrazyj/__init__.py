5631d4fed1847ff2dd7453453457867958cc0a27
crazyj <crazyj7@gmail.com>
2019-10-01 16:24:05 +0900

    pack

--- mypackcrazyj/mypackcrazyj/__init__.py
+++ mypackcrazyj/mypackcrazyj/__init__.py
@@ -1,6 +1,8 @@
 
 from .text import joke
 
+name = "mypackcrazyj"
+
 
 def sum(a, b):
     return a+b
--- mypackcrazyj_test/mypacktest.py
+++ mypackcrazyj_test/mypacktest.py
@@ -5,6 +5,8 @@ mypack test
 
 import mypackcrazyj
 
+print (mypackcrazyj.name)
+
 print(mypackcrazyj.sum(1,2))
 
 print(mypackcrazyj.text.joke())
